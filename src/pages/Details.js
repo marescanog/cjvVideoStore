bad069c1724372b889a7142b5e13081559fcf0d2
marescanog <marescanog@gmail.com>
2024-03-08 04:46:06 -0500

    added package and stuff

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+import { Rating } from 'react-simple-star-rating'
 import { useParams } from "react-router-dom";
 import '../styles/Details.css';
 
@@ -35,19 +36,50 @@ const Details = () => {
                 <Image src={details?.posterImage} fluid className='details-poster-img'/>
               </div>
               <div className='details-info-right'>
-                <h3>{details?.title}</h3>
+                <h2>{details?.title}</h2>
 
                 <div className='genre-rating-section'>
-                  <div>
-                    {JSON.stringify(details?.genre)}
+                  {
+                    details?.genre && details?.genre.length > 0 ? (
+                      details?.genre.map((el,index, arr)=>{
+                        return <p className="genre-text-style">{el} {index!=arr.length-1?"|":""}</p>
+                      })
+                    )
+                    : <></>
+                  }
+                  <p className="genre-text-style">| {details?.releaseYear}</p>
+                  
+                  <div className="mrating-box-style">
+                    <p className="mrating-text-style">{details?.MPARating}</p>
                   </div>
-                  <div>
-                    {JSON.stringify(details?.MPARating)}
-                  </div>
+                  { details?.Length && 
+                    <div className="mrating-box-style">
+                      <p className="mrating-text-style">{`${details?.Length} min`}</p>
+                    </div>
+                  }
+                  { details?.numOfSeasons && 
+                    <div className="mrating-box-style">
+                      <p className="mrating-text-style">S1-S{details?.numOfSeasons}</p>
+                    </div>
+                  }
+                  { details?.numOfTotalEpisodes && 
+                    <div className="mrating-box-style">
+                      <p className="mrating-text-style">Ep1-Ep{details?.numOfTotalEpisodes}</p>
+                    </div>
+                  }
                 </div>
 
                 <div className='rating-section'>
-                  {JSON.stringify(details?.rating)}
+                  <div className='rating-component-container'>
+                    <Rating onClick={()=>{}}
+                      initialValue={Math.floor((details?.rating)/2??0)}
+                      iconsCount={5}
+                      disableFillHover={true}
+                      SVGclassName={'rating-svg'}
+                    />
+                  </div>
+                  <div className='rating-text-container'><p className='rating-text-style'>({details?.totalRatings})</p></div>
+
                 </div>
 
                 <div className='buttons-section'>
@@ -61,8 +93,8 @@ const Details = () => {
                 </div>
 
                 <div>
-                  <div>Rent       {JSON.stringify(details?.rentPrice)}</div>
-                  <div>Buy       {JSON.stringify(details?.buyPrice)}</div>
+                  <Button variant="info" >Rent       {JSON.stringify(details?.rentPrice)}</Button>
+                  <Button variant="info" >Buy       {JSON.stringify(details?.buyPrice)}</Button>
                 </div>
               </div>
             </div>
